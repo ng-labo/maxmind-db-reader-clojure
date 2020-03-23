3a7ed73e7a627a9ca9a99cf3ab633ae37255f56f
ng-labo <n-ogawa@sakura.ad.jp>
2020-03-23 10:37:28 +0900

    fix the coding style

--- src/maxmind_db_reader_clojure/core.clj
+++ src/maxmind_db_reader_clojure/core.clj
@@ -1,18 +1,16 @@
 (ns maxmind-db-reader-clojure.core
-  (:import com.maxmind.db.Reader)
-  (:import com.maxmind.db.NoCache)
-  (:import java.io.File)
-  (:import java.net.InetAddress) 
+  (:import [com.maxmind.db Reader NoCache])
+  (:import [com.maxmind.db Reader$FileMode])
+  (:import [java.io File])
+  (:import [java.net InetAddress])
 )
 
 (defn lookup [ path-to-mmdb ipaddress ]
   (let [ans (.get
-            (new com.maxmind.db.Reader
-              (new java.io.File path-to-mmdb)
-              com.maxmind.db.Reader$FileMode/MEMORY_MAPPED
-              (com.maxmind.db.NoCache/getInstance))
-            (java.net.InetAddress/getByName ipaddress))]
-    (if (some? ans) (.toString ans) nil)
-  )
-)
+              (Reader.
+                (File. path-to-mmdb)
+                Reader$FileMode/MEMORY_MAPPED
+                (NoCache/getInstance))
+              (InetAddress/getByName ipaddress))]
+    (if (some? ans) (.toString ans) nil)))
 
